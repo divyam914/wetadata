f7884ed5579e5174d66f3cfe09e65d56b0a2f175
Ellie Czepiel <ellieczepiel@Ellies-MacBook-Pro.local>
2018-01-12 11:03:21 -0500

    python donegit add analysis.py and app.jsx and home.jsx started

---
+++ views/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App;
