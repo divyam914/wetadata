4584a49f8d27c1058b221186b594319ca32005f1
Brandon <bw1126@nyu.edu>
2018-01-13 18:24:21 -0500

    yeah boiiiiiii we got a react component to work

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import React from 'react';
+import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.jsx';
+                                                                                                                                                                                                                                                  
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <p> Hello React Project</p>
+        <AwesomeComponent />
+      </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
